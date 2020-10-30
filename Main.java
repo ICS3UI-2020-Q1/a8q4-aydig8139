2b895fa97f29fd3925656fec8c28331bfccf96a2
aydig8139 <aydig8139@wrdsb.ca>
2020-10-30 14:06:44 +0000

    finished factorial question

--- Main.java
+++ Main.java
@@ -6,6 +6,15 @@ public class Main implements Runnable, ActionListener{
 
   // Class Variables  
   
+  JPanel mainPanel;
+  
+  JLabel nLabel;
+  JLabel factorialLabel;
+
+  JTextField input;
+  JTextField output;
+
+  JButton calcButton;
 
 
   // Method to assemble our GUI
@@ -18,7 +27,54 @@ public class Main implements Runnable, ActionListener{
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
- 
+
+    //initialize the main JPanel
+    mainPanel = new JPanel();
+
+    //disable any layout helpers
+    mainPanel.setLayout(null);
+
+    //create the labels
+    nLabel = new JLabel("n");
+    factorialLabel = new JLabel("factorial(n)");
+
+    //layout the labels by setting the coordinate and size 
+    nLabel.setBounds(20,20,20,20);
+    factorialLabel.setBounds(120,20,80,20);
+
+    //initialize the input text fields
+    input = new JTextField();
+    output = new JTextField();
+
+    //set the location and size 
+    input.setBounds(50, 20, 50, 20);
+    output.setBounds(210, 20, 100, 20);
+
+    //initialize the button
+    calcButton = new JButton("CALC");
+
+    //set the size and location of the button
+    calcButton.setBounds(320, 20, 80, 20);
+
+    //add an action listener to the buttons
+    calcButton.addActionListener(this);
+
+    //set the action command on the buttons
+    calcButton.setActionCommand("CALC");
+
+    //add the button to the panel
+    mainPanel.add(calcButton);
+
+    //add the text fields to the panel
+    mainPanel.add(input);
+    mainPanel.add(output);
+
+    //add the labels to the main panel 
+    mainPanel.add(nLabel);
+    mainPanel.add(factorialLabel);
+
+    //add the panel to the window
+    frame.add(mainPanel);
     
 
   }
@@ -28,6 +84,25 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
+    if(command.equals("CALC")){
+
+      //get text from input and change it into an integer
+      String inputText = input.getText();
+      int inputInt = Integer.parseInt(inputText);
+      
+      //declare variable to store the product of the numbers multiplied
+      int product = 1;
+
+      //create for loop and declare variable to decrease the multiple number one by one
+      for(int multiply = inputInt; multiply >= 1; multiply--){
+      
+      product = product*multiply;
+    }
+
+    //print result on screen
+    output.setText("" + product);
+
+  }
   }
 
   // Main method to start our program
